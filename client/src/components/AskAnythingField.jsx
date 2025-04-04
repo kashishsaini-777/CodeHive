51f6c9d82fbad2f3d3e89f97dbb5cef67e642a4f
Akshita1905 <akshita19005@gmail.com>
2025-04-05 01:53:13 +0530

    Updated DoubtCard.css , DoubtCreation.css , Navbar.css , AskAnythingField.css

--- client/src/components/AskAnythingField.jsx
+++ client/src/components/AskAnythingField.jsx
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './AskAnythingField.css';
 
 const AskAnythingField = ({ onClick }) => {
+  const [question, setQuestion] = useState('');
+
   return (
     <div className="ask-container" onClick={onClick}>
       <input 
         type="text" 
         placeholder="Ask anything..." 
         className="ask-input"
-        readOnly
+        value={question}
+        onChange={(e) => setQuestion(e.target.value)}
       />
     </div>
   );
